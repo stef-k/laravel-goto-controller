b7030046f7d2dbded7ef23beaa91a4c859c40c74
Antony Kurniawan <smankusors@icloud.com>
2018-06-28 15:27:28 +0700

    Added goto line, fixed search scheme

--- src/link.ts
+++ src/link.ts
@@ -4,7 +4,7 @@ import { workspace, Position, Range, CancellationToken, DocumentLink, DocumentLi
 import * as util from './util';
 
 export class LinkProvider implements DocumentLinkProvider {
-  /**
+    /**
    * provideDocumentLinks
    */
   public provideDocumentLinks(document: TextDocument, token: CancellationToken): ProviderResult<DocumentLink[]> {
@@ -16,11 +16,13 @@ export class LinkProvider implements DocumentLinkProvider {
       let result = line.text.match(reg);
       if (result != null) {
         for (let item of result) {
-          let filePath = util.getFilePath(item, document);
+          let splitted = item.replace(/\"|\'/g, '').split('@');
+          if (splitted.length != 2) continue;
+          let filePath = util.getFilePath(splitted[0], document);
           if (filePath != null) {
             let start = new Position(line.lineNumber, line.text.indexOf(item) + 1);
             let end = start.translate(0, item.length - 2);
-            let documentLink = new DocumentLink(new Range(start, end), Uri.file(filePath));
+            let documentLink = new util.LaravelControllerLink(new Range(start, end), filePath, splitted[0], splitted[1]);
             documentLinks.push(documentLink);
           }
         }
@@ -28,5 +30,18 @@ export class LinkProvider implements DocumentLinkProvider {
       index++;
     }
     return documentLinks;
+  }
+
+  /**
+   * resolveDocumentLink
+   */
+  public resolveDocumentLink(link: util.LaravelControllerLink, token: CancellationToken): ProviderResult<DocumentLink> {
+    let lineNum = util.getLineNumber(link.funcName, link.filePath);
+    let path = link.filePath;
+    if (lineNum != -1)
+        path += "#" + lineNum;
+
+    link.target = Uri.parse("file:///" + path);
+    return link;
   }
 }
